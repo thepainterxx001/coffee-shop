f12d82d23240373fd52a08486fa608abdbce8cad
Emdi Gemini <yougotjinx@gmail.com>
2026-03-17 20:45:24 +0800

    Install backend dependencies

--- system-admin/frontend/src/main.jsx
+++ system-admin/frontend/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <App />
+      <Toaster  />
+    </Router>
   </StrictMode>,
 )
